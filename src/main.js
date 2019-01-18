ee540ad2250d4ea9084b5c36ca400b5297738b24
Valeyev Rustam <snooks87@gmail.com>
2019-01-18 17:27:53 +0300

    Authentication

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink } from 'apollo-link'
 import 'tachyons'
 import Vue from 'vue'
 // 2
@@ -11,6 +12,8 @@ import VueApollo from 'vue-apollo'
 
 import App from './App'
 import router from './router'
+
+import { GC_USER_ID, GC_AUTH_TOKEN } from './constants/settings'
 
 Vue.config.productionTip = false
 
@@ -20,9 +23,21 @@ const httpLink = new HttpLink({
   uri: 'https://api.graph.cool/simple/v1/cjr1pdmvs066v0163uxqlh0pq'
 })
 
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  const token = localStorage.getItem(GC_AUTH_TOKEN)
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : null
+    }
+  })
+
+  return forward(operation)
+})
+
 // 4
 const apolloClient = new ApolloClient({
-  link: httpLink,
+  link: authMiddleware.concat(httpLink),
   cache: new InMemoryCache(),
   connectToDevTools: true
 })
@@ -38,11 +53,16 @@ const apolloProvider = new VueApollo({
   }
 })
 
+let userId = localStorage.getItem(GC_USER_ID)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   provide: apolloProvider.provide(),
   router,
+  data: {
+    userId
+  },
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-// 1
 import CreateLink from '../components/CreateLink'
 import LinkList from '../components/LinkList'
+import AppLogin from '../components/AppLogin'
 
 Vue.use(Router)
 
 export default new Router({
-  // 2
   routes: [
     {
       path: '/',
@@ -17,8 +16,11 @@ export default new Router({
     {
       path: '/create',
       component: CreateLink
+    },
+    {
+      path: '/login',
+      component: AppLogin
     }
   ],
-  // 3
   mode: 'history'
 })
