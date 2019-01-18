fb868b7295fd32d7a6a6752c778ff583ea27ead0
Valeyev Rustam <snooks87@gmail.com>
2019-01-18 12:11:41 +0300

    queries: loading links

---
+++ src/components/LinkList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h4 v-if="loading">Loading...</h4>
+    <link-item
+      v-for="link in allLinks"
+      :key="link.id"
+      :link="link">
+    </link-item>
+  </div>
+</template>
+
+<script>
+import { ALL_LINKS_QUERY } from '../constants/graphql'
+import LinkItem from './LinkItem'
+
+export default {
+  name: 'LinkList',
+  components: {
+    LinkItem
+  },
+  data () {
+    return {
+      allLinks: [],
+      loading: 0
+    }
+  },
+  apollo: {
+    allLinks: {
+      query: ALL_LINKS_QUERY
+    }
+  }
+}
+</script>
